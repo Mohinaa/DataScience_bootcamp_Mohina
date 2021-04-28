20d4cc8a0fbe368fb447e874e5237c4de4acb88f
Mohinaa <mohinaa01@gmail.com>
2021-04-27 20:45:59 -0400

    Question 2
    Test

--- Question_1.sql
+++ Question_1.sql
@@ -2,3 +2,9 @@ Select metro_city, avg (median_hh_income) as avg_cost
 From public.census_metro_data cmd
 Group by 1
 Order by 2 desc
+
+Select metro_city, max (population) as max_pop
+From public.census_metro_data cmd
+Group by 1
+Order by 2 desc
+
