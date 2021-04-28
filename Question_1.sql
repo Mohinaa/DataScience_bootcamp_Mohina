b32f822661c2933691235d2dd87dc4c2f7c03495
Mohinaa <mohinaa01@gmail.com>
2021-04-27 20:52:04 -0400

    Question 3
    Test

--- Question_1.sql
+++ Question_1.sql
@@ -3,8 +3,21 @@ From public.census_metro_data cmd
 Group by 1
 Order by 2 desc
 
+---Bridgeport
+
 Select metro_city, max (population) as max_pop
 From public.census_metro_data cmd
 Group by 1
 Order by 2 desc
+
+---Houston
+
+Select state, count (distinct metro_city) 
+As total_metros
+From public.census_metro_data cmd
+Group by 1 
+Order by 2 desc
+
+--California
+
 
